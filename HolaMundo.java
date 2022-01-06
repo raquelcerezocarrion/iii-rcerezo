a860cab3fe2a2e7a801a1ba446cb3ce1c5531e5d
Raquel Cerezo <raquel.cerezocarrion@usp.ceu.es>
2022-01-06 19:27:29 +0100

    Nuevo método printCourse en HolaMundo
    Llamada a imprimir nombre de la asignatura en HolaMundo

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,11 @@
 public class HolaMundo{
-public static void main(String[] args){ System.out.println("Otro Hola mundo"); }}
+public static void main(String[] args){
+System.out.println("Otro Hola mundo");
+printCourse();
+}
+public static void printCourse(){ System.out.println("Introducción Ingenieria Informática");}
+}
+
 
 
 
