a10e340f7371930e2da65507d50ba777668f4acc
raquelcerezocarrion <raquel.cerezocarrion>
2024-05-27 00:26:20 +0200

    Tests del servidor

--- Simulacro/test_server.py
+++ Simulacro/test_server.py
@@ -15,5 +15,20 @@ class TestServer(unittest.TestCase):
             s.sendall("FECHA".encode('utf-8'))
             response = s.recv(1024).decode('utf-8')
             self.assertRegex(response, r"\d{4}-\d{2}-\d{2}")
+
+    def test_hora(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect(('localhost', 16031))
+            s.sendall("HORA".encode('utf-8'))
+            response = s.recv(1024).decode('utf-8')
+            self.assertRegex(response, r"\d{2}:\d{2}:\d{2}")
+
+    def test_unknown_message(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect(('localhost', 16031))
+            s.sendall("HELLO".encode('utf-8'))
+            response = s.recv(1024).decode('utf-8')
+            self.assertEqual(response, "ERROR")
+
 if __name__ == "__main__":
     unittest.main()
